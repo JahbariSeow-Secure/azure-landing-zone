87c91d91b708e57096357772951d23a33d7187fe
Jahbari Seow <jbseow0@gmail.com>
2026-04-09 11:38:40 -0400

    Update providers.tf

--- azure-landing-zone-lab/providers.tf
+++ azure-landing-zone-lab/providers.tf
@@ -1,4 +1,4 @@
-﻿terraform {
+terraform {
   required_version = ">= 1.6.0"
   required_providers {
     azurerm = {
@@ -6,8 +6,15 @@
       version = ">= 3.80.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform-state"
+    storage_account_name = "stterraformstate8274"
+    container_name       = "tfstate"
+    key                  = "landing-zone.tfstate"
+  }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "7b3cde72-e4eb-4411-bab1-dcd81a2f7ca3"
 }
