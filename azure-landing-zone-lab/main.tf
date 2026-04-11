3ea1c5fb7107faf2a4a4ecd40f7933141d49f1b6
Jahbari Seow <jbseow0@gmail.com>
2026-04-10 23:21:37 -0400

    Update main.tf

--- azure-landing-zone-lab/main.tf
+++ azure-landing-zone-lab/main.tf
@@ -31,3 +31,15 @@ module "monitoring" {
   management_group_id = module.management_groups.id
   alert_email         = "leeseow12@gmail.com"
 }
+module "key_vault" {
+  source                     = "./modules/key-vault"
+  key_vault_name             = "kv-landing-zone-lab"
+  location                   = "eastus"
+  resource_group_name        = "rg-hub"
+  pipeline_principal_id      = "27931c5f-125f-4bb8-85a2-d1f412912974"
+  admin_principal_id         = "adf1cf26-8411-4be4-b76c-16ad514df6ad"
+  subnet_id                  = module.networking.hub_subnet_id
+  log_analytics_workspace_id = module.monitoring.workspace_id
+  subscription_id            = "/subscriptions/7b3cde72-e4eb-4411-bab1-dcd81a2f7ca3"
+  action_group_id            = module.monitoring.action_group_id
+}
