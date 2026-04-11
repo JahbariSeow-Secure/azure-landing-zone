880003a8f8bd60df3e4de07ae93bfa583c8a7504
Jahbari Seow <jbseow0@gmail.com>
2026-04-10 23:15:49 -0400

    Update variables.tf

--- azure-landing-zone-lab/modules/key-vault/variables.tf
+++ azure-landing-zone-lab/modules/key-vault/variables.tf
@@ -1,1 +1,37 @@
 
+variable "key_vault_name" {
+  type = string
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "pipeline_principal_id" {
+  type = string
+}
+
+variable "admin_principal_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "action_group_id" {
+  type = string
+}
